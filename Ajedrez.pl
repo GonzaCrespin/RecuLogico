6483c39ff0252087804cfa96291b453b138ab9e0
GonzaCrespin <gonzacrespin@gmail.com>
2023-12-20 19:25:46 -0300

    1 Commit

---
+++ Ajedrez.pl
@@ -0,0 +1,13 @@
+nro(1).
+nro(2).
+nro(3).
+nro(4).
+nro(5).
+nro(6).
+nro(7).
+nro(8).
+
+% pieza(nombre,color,ubicacionY,ubicacionX).
+pieza(torre, negra, 2,1).
+pieza(torre, negra, 3,4).
+pieza(reina, negra, 5,5).
