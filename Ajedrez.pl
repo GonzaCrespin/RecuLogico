bd57fdd1bbdcc76e1ff53088bf54b6dc866beba4
GonzaCrespin <gonzacrespin@gmail.com>
2023-12-20 19:53:35 -0300

    Agrego Punto 2 y 3

--- Ajedrez.pl
+++ Ajedrez.pl
@@ -11,6 +11,8 @@ nro(8).
 pieza(torre, negra, 2,1).
 pieza(torre, negra, 3,4).
 pieza(reina, negra, 5,5).
+pieza(reina, negra, 2,1).
+pieza(reina,negra,10,10).
 
 % Encontrar los casilleros libres, es decir las posiciones del tablero donde no hay ninguna ficha
 
@@ -18,3 +20,28 @@ casilleroLibre(UbiY,UbiX):-
     nro(UbiX),
     nro(UbiY),
     not(pieza(_,_,UbiY,UbiX)).
+
+% Detectar si hay información errónea, como ser un casillero ocupado por dos piezas a la vez, una pieza en un casillero inválido,
+% algun elemento extraño en un casillero.
+
+infoErronea(UbiY,UbiX):-
+    pieza(Pieza1,_,UbiY,UbiX),
+    pieza(Pieza2,_,UbiY,UbiX),
+    Pieza2 \= Pieza1.
+
+infoErronea(UbiY, UbiX) :-
+    not(nro(UbiY)), not(nro(UbiX)),
+    pieza(_,_,UbiY,UbiX).
+
+% El movimiento de la torre es en líneas rectas, ya sea por filas o columnas.
+% Encontrar todos los casilleros a los que puede moverse la torre de una posicion dada.
+% Tienen que estar libres u ocupados por una pieza del otro color.
+% (No hace falta contemplar que tenga el camino libre, sólo el casillero de llegada)
+% No se puede mover al mismo casillero donde está originalmente la torre. 
+
+moverTorre(UbiYActual,UbiXActual,UbiYDestino,UbiXDestino):-
+    dif((UbiXActual, UbiYActual), (UbiXDestino, UbiYDestino)),
+    nro(UbiXActual),nro(UbiYActual),
+    nro(UbiXDestino),nro(UbiYDestino),
+    (UbiYActual = UbiYDestino; UbiXActual = UbiXDestino),
+    not(pieza(_, Color, UbiYDestino, UbiXDestino)).
